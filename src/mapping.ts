d5db94252f9ede09d6c6dff215f8f8fcee405a7d
Enrique Ortiz <hi@enriqueortiz.dev>
2020-07-11 10:07:38 -0400

    Initialize subgraph

---
+++ src/mapping.ts
@@ -0,0 +1,25 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { AddPolicy, ExecuteIssuance, RemovePolicy } from "../generated/Issuance/Issuance"
+import { ExampleEntity, IssuancePolicy } from "../generated/schema"
+
+export function handleAddPolicy(event: AddPolicy): void {
+  let entity = IssuancePolicy.load(event.params.policyId.toString())
+  if (entity) {
+    return
+  }
+  entity = new IssuancePolicy(event.params.policyId.toString())
+  entity.beneficiary = event.params.beneficiary
+  entity.blockInflationRate = event.params.blockInflationRate
+  entity.active = true
+  entity.save()
+}
+
+export function handleRemovePolicy(event: RemovePolicy): void {
+  let entity = IssuancePolicy.load(event.params.policyId.toString())
+  if (!entity) {
+    return
+  }
+  entity.active = false
+  entity.save()
+}
+
